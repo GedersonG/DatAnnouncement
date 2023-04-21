241c456c31da63f56761d43ce9c135965a43e779
Gederson <gedersongustavogbot@ufps.edu.co>
2023-04-20 19:40:17 -0500

    Master key permit in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  # config.hosts << "datannouncement-production.up.railway.app"
+  config.hosts << "datannouncement-production.up.railway.app"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
