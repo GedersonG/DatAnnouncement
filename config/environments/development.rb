1d01ed0265f0792e77613d6f7017681eacadb9b7
Gederson <gedersongustavogbot@ufps.edu.co>
2023-05-07 13:04:48 -0500

    Testing production environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   # Host to production in railway
-  # config.hosts << "datannouncement-production.up.railway.app"
+  config.hosts << "datannouncement-production.up.railway.app"
   # Do not eager load code on boot.
   config.eager_load = false
 
